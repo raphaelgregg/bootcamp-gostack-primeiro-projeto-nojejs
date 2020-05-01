ffbce145a60a06ac45099e2af9356e8c49652e29
Raphael Gregg <raphaelgregg@gmail.com>
2020-05-01 01:04:03 -0300

    first commit

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,49 @@
+import { startOfHour } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+/**
+ * [x] Recebimentos de informações
+ * [x] Tratativa de erro/excessões
+ * [x] Acesso ao Repositório
+ */
+
+interface RequestDTO {
+  provider: string;
+  date: Date;
+}
+
+/**
+ * (SOLID)
+ * Single Responsability Principle
+ * Dependency Inversion Principle
+ */
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ provider, date }: RequestDTO): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findbyDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
